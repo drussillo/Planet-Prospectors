b7aad84976af3a45f6022fdb6bb9bfa5f1dbc047
drussillo <daverussillo7@gmail.com>
2026-03-03 18:28:20 -0600

    Actually added it this time

---
+++ planet-prospectors/scripts/planet1/chunk.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var chunk_width
+var chunk_length
+var mat
+
+
+func set_chunk(x, z, width, length) -> void:
+	mat = $floor.get_surface_override_material(0).duplicate()
+	$floor.set_surface_override_material(0, mat)
+	chunk_width = width
+	chunk_length = length
+	position.x = x
+	position.z = z
+	scale = Vector3(chunk_width, 1, chunk_length)
+	mat.uv1_scale = Vector3(chunk_width, chunk_length, 1) / 4
+	
+	mat.albedo_color = Color(randf(), randf() ,randf()) # debug
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
