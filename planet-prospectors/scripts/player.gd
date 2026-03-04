ee8e746ae9b7faafc6cf0d9d7b4c4e2cd5cb6ea3
drussillo <daverussillo7@gmail.com>
2026-03-03 19:00:33 -0600

    Added player camera bobbing effect

--- planet-prospectors/scripts/player.gd
+++ planet-prospectors/scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENS = 0.001
+
+var head_bob_speed = 0.01
+@onready var head_distance = $Head.global_position.y - global_position.y
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -35,8 +38,14 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if is_on_floor():
+			var distance = abs($Head.global_position.y - global_position.y - head_distance)
+			if distance > head_distance/12:
+				head_bob_speed *= -1
+			$Head.position.y += head_bob_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		if is_on_floor():
+			$Head.position.y = global_position.y + head_distance
 	move_and_slide()
