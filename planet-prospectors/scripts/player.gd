8f854098dac67cc737aa38913da984a244e1027f
drussillo <daverussillo7@gmail.com>
2026-03-07 21:23:42 -0600

    Oil chunks - Added oil chunks - Added chunk and oil chunk groups and tracking - Added oil collection - Added drill scene (still very conceptual)

--- planet-prospectors/scripts/player.gd
+++ planet-prospectors/scripts/player.gd
@@ -1,15 +1,24 @@
 extends CharacterBody3D
 
 
-const SPEED = 4.0
+#const SPEED = 4.0
+const SPEED = 40.0 # debug speed
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENS = 0.001
+
+const DRILL_SCENE = preload("res://scenes/drill.tscn")
 
 var head_bob_speed = 0.01
 @onready var head_distance = $Head.global_position.y - global_position.y
 
+var current_chunk
+var current_oil = 0
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# TODO: spawn on surface / spawn on rocket, then land
+	#position.y = 1000
+	#velocity.y = -500
 	
 func _input(event) -> void:
 	if event is InputEventMouseMotion:
@@ -48,4 +57,19 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		if is_on_floor():
 			$Head.position.y = global_position.y + head_distance
+	
+	if Input.is_action_just_pressed("interact"):
+		#var drill = DRILL_SCENE.instantiate()
+		#drill.position = position
+		#add_sibling(drill)
+		pass
+		# TODO: place drill
+		#print(get_node("Head/PlayerAim").target_position)
+	
+	if current_chunk != null:
+		if Input.is_action_pressed("interact") && current_chunk.oil_amount > 0:
+			current_chunk.oil_amount -= 1;
+			current_oil += 1;
+	
+		print(current_chunk.oil_amount, "  ", current_oil)
 	move_and_slide()
