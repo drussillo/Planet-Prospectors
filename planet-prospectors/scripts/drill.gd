580dcf0f7476d5b9ab58b479d1bf415f4c415742
drussillo <daverussillo7@gmail.com>
2026-03-12 11:04:32 -0500

    Added drill asset and animation

--- planet-prospectors/scripts/drill.gd
+++ planet-prospectors/scripts/drill.gd
@@ -15,4 +15,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if chunk.oil_amount > 0:
+		rotation.y += 20 * delta
+		# TODO: play drilling sound
